fc6a4e42f285576d50d6cdf899e5bda4727b9961
muansuiu <mnezami173053@bscse.uiu.ac.bd>
2022-09-21 00:51:40 +0600

    added get app.get() method to start with a express server

--- myApp.js
+++ myApp.js
@@ -1,5 +1,13 @@
 let express = require('express');
+//app.get("Hello Express");
+
 let app = express();
+app.get("/",function(req,res)
+{
+  res.send("Hello Express");
+});
+
+
 console.log("Hello World");
 
 
